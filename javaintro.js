ea01fcc40bdf7b4a3868812d887330b500781520
Emily Berger <emilyberger@gbg-wireless-pittnet-150-212-104-85.wireless.pitt.edu>
2018-11-28 10:13:36 -0500

    homework Javascript

---
+++ javaintro.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoad', init, false)
+
+function init() {
+    alert ('Did the page load? Yes!');
+var buttons = document.getElementByTagName ("button")
+
+var butt
+    
